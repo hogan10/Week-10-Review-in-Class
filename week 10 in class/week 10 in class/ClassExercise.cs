85961bb853d0740988a05dd2adbad9685e88eba2
Erin Hogan <hogan10@charter.net>
2018-09-12 19:52:40 -0500

    updated

--- week 10 in class/week 10 in class/ClassExercise.cs
+++ week 10 in class/week 10 in class/ClassExercise.cs
@@ -9,18 +9,27 @@ namespace week_10_in_class
         public void Run()
         {
             List<Student> students = CreateStudentList();
+
+            //to add a new student:
+
+            Student newStudent = new Student(6, "Jane", 24, 3.0F);
+            //the 0 makes sure that this new student inserts at the first index and pushes the rest to the next index
+            //you could put 1 2 or 3 to move it around to whichever spot you want. can't do this with an array bc it would replace that item instead of pushing it 
+            students.Insert(0, newStudent);
+
             //use below if you are using tostring in main
             PrintStudentInfo(students);
 
             // OTHER EXAMPLE OF CREATING LIST List<Student> students2 = new List<Student>();
             // students2.Add(student1);  (DO THIS FOR EACH STUDENT)
 
-            //do the below if you don't want to use the to string return in the main 
+            //do the below if you don't want to use the to string to return everything at once in the main 
             //PrintId(students);
             //PrintName(students);
 
         }
-
+        //use the first method below to show all student info when using the string to in the main 
+        //if you don't use the string to use individual methods below to call individually (age, gpa, name, id)
         private void PrintStudentInfo(List<Student> students)
         {
             foreach (Student student in students)
